cff27b209df2ddd4b94569f2da3e2d14df97af3e
Wojciech Mista <wojciech.mista@hotmail.com>
2019-02-03 15:44:28 +0100

    Added controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,40 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class Controller {
+
+    @FXML
+    private LineChart<?, ?> utChart;
+
+    @FXML
+    private LineChart<?, ?> secondChart;
+
+    @FXML
+    private Text freq;
+
+    @FXML
+    private Text max;
+
+    @FXML
+    private TextField paramOne;
+
+    @FXML
+    private TextField paramTwo;
+
+    @FXML
+    private TextField paramThree;
+
+    @FXML
+    private TextField paramFour;
+
+    @FXML
+    void runPressed(ActionEvent event) {
+
+    }
+
 }
